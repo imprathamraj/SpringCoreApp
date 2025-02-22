919c54c3eb1cd43af17247e767f400da25d17782
imprathamraj <rjpratham05@gmail.com>
2025-02-22 11:10:08 +0530

    [Pratham Raj] Add. Using Employee Bean and Department Bean Dependency Injection

---
+++ src/main/java/com/spring/appdevelopment/Bean/DepartmentBean.java
@@ -0,0 +1,11 @@
+package com.spring.appdevelopment.Bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DepartmentBean {
+    public String departmentName = "Software Department";
+    public String getDepartmentName(){
+        return departmentName;
+    }
+}
